73e9f8275111477e532a565debf54fe5c21eda2c
upyakenyaorg <upya.kenyaorg@gmail.com>
2026-09-24 20:27:46 +0300

    Delete site.js

--- site.js
+++
@@ -1,3 +0,0 @@
-
-const menu=document.querySelector('.menu'),nav=document.querySelector('nav');
-if(menu){menu.addEventListener('click',()=>{nav.classList.toggle('open');menu.textContent=nav.classList.contains('open')?'Close':'Menu';});}
